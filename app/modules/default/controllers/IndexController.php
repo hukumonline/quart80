55b903ced02a39d416f74345c6e2bc4215b691ed
nihki <nihki@hukumonline.com>
2011-07-04 19:17:40 +0700

    modified device mobile

--- app/modules/default/controllers/IndexController.php
+++ app/modules/default/controllers/IndexController.php
@@ -9,16 +9,23 @@ class IndexController extends Zend_Controller_Action
 	function indexAction()	
 	{
 		$tw = Zend_Registry::get('twurfl');
-        if(!$tw->getDeviceCapability("is_wireless_device")){
-        	$this->_forward('index','index','hol-site');
-       	}
-        else
-        {
+		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
+		$cap = $tw->capabilities;
+		
+		// check if this device is mobile
+		if($cap['product_info']['is_wireless_device']){
+			
 		    $registry = Zend_Registry::getInstance();
 		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
 		    $cdn = $config->getOption('mobile');
 			$this->_redirect($cdn['url']);	        
-       	}
+			
+		}else{
+			
+			$this->_forward('index','index','hol-site');			
+			
+		}
+       	
 	}
 }
 
