c37a2debb5446c1e5e26a35ef46a2a1d08e3b2de
nihki <nihki@hukumonline.com>
2011-07-06 15:57:29 +0700

    modified index mobile site

--- app/modules/default/controllers/IndexController.php
+++ app/modules/default/controllers/IndexController.php
@@ -21,7 +21,8 @@ class IndexController extends Zend_Controller_Action
 		    
 		    $uri = $_SERVER['REQUEST_URI'];
 		    
-			$this->_redirect($cdn['url'].$uri);	        
+			//$this->_redirect($cdn['url'].$uri);	        
+			header("Location:".$cdn['url'].$uri);
 			
 		}else{
 			
