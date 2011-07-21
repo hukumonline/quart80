4e7db243000b83d5842213689cfe6a284b6d23e2
nihki <nihki@hukumonline.com>
2011-07-21 13:42:28 +0700

    modified detail klinik

--- app/modules/klinik/controllers/BrowserController.php
+++ app/modules/klinik/controllers/BrowserController.php
@@ -13,8 +13,28 @@ class Klinik_BrowserController extends Zend_Controller_Action
 	}
 	function detailAction()
 	{
-		$catalogGuid = ($this->_getParam('guid'))? $this->_getParam('guid') : '';
-		$this->view->catalogGuid = $catalogGuid;
+		$tw = Zend_Registry::get('twurfl');
+		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
+		$cap = $tw->capabilities;
+		
+		// check if this device is mobile
+		if($cap['product_info']['is_wireless_device']){
+			
+		    $registry = Zend_Registry::getInstance();
+		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
+		    $cdn = $config->getOption('mobile');
+		    
+		    $uri = $_SERVER['REQUEST_URI'];
+		    
+			//$this->_redirect($cdn['url'].$uri);	        
+			header("Location:".$cdn['url'].$uri);
+			
+		}else{
+			
+			$catalogGuid = ($this->_getParam('guid'))? $this->_getParam('guid') : '';
+			$this->view->catalogGuid = $catalogGuid;
+		
+		}
 	}
 	function categoryAction()
 	{
