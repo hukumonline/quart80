74477db8fb566563f03fa3eba55594644e4fe178
nihki <nihki@hukumonline.com>
2011-07-21 13:46:38 +0700

    modified index klinik

--- app/modules/klinik/controllers/IndexController.php
+++ app/modules/klinik/controllers/IndexController.php
@@ -11,7 +11,26 @@ class Klinik_IndexController extends Zend_Controller_Action
 		$this->_helper->layout->setLayout('layout-klinik');
 		$this->_helper->layout->setLayoutPath(array('layoutPath' => ROOT_DIR.'/app/modules/klinik/layouts'));
 	}
-	function indexAction(){}
+	function indexAction()
+	{
+		$tw = Zend_Registry::get('twurfl');
+		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
+		$cap = $tw->capabilities;
+		
+		// check if this device is mobile
+		if($cap['product_info']['is_wireless_device']){
+			
+		    $registry = Zend_Registry::getInstance();
+		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
+		    $cdn = $config->getOption('mobile');
+		    
+		    $uri = $_SERVER['REQUEST_URI'];
+		    
+			//$this->_redirect($cdn['url'].$uri);	        
+			header("Location:".$cdn['url'].$uri);
+			
+		}		
+	}
 	function disclaimerAction(){}
 	function browseAction() 
 	{
